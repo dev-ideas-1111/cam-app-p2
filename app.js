472554818f61b935beac39d01f16864309eeca24
Dev Ideas <60462155+dev-ideas-1111@users.noreply.github.com>
2020-06-14 15:44:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// Set constraints for the video stream
+//var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: "environment", width: {min: 1280}, height: {min: 720} }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraCanvas = document.querySelector("#camera--canvas"),
+    cameraTrigger = document.querySelector("#camera--button");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraCanvas.width = cameraView.videoWidth;
+    cameraCanvas.height = cameraView.videoHeight;
+    cameraCanvas.getContext("2d").drawImage(cameraView, 0, 0, cameraCanvas.width, cameraCanvas.height);
+    
+    cameraOutput.src = cameraCanvas.toDataURL("image/png");
+    cameraOutput.classList.add("taken");
+    //track.stop();
+    
+    
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
